4340e8373e7cd440b8d8511f1e39622553143da9
anxmesh47 <112456607+anxmesh47@users.noreply.github.com>
2023-09-18 02:51:35 -0700

    Update forLogin.js

--- forLogin.js
+++ forLogin.js
@@ -1,8 +1,29 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 5500;
+const port = 3000; // You can change the port number
 
-app.get("/", (req, res) => {
-  res.status(5500).send(0);
+// Middleware to parse JSON and form data
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Serve static HTML files from a directory named 'public'
+app.use(express.static('public'));
+
+// Define a route for handling the login form submission
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    // Perform authentication here (e.g., check against a database)
+    // Replace this with your actual authentication logic
+    if (username === 'your_username' && password === 'your_password') {
+        res.send('Login successful'); // Redirect or respond accordingly
+    } else {
+        res.send('Login failed'); // Redirect or respond accordingly
+    }
 });
-app.listen(port, () => console.log("Server has started on port: $(port}"));
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
