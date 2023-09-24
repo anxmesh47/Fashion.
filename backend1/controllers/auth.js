7e3950d165b3f5b19b52ef4a3ea4f70230753f98
anxmesh47 <112456607+anxmesh47@users.noreply.github.com>
2023-09-24 11:24:30 +0545

    Update auth.js

--- backend1/controllers/auth.js
+++ backend1/controllers/auth.js
@@ -10,7 +10,7 @@ const sendgridTransport = require("nodemailer-sendgrid-transport");
 const transporter = nodemailer.createTransport(
   sendgridTransport({
     auth: {
-      // api_key: Api key goes here..
+      // api_key: ________________
     }
   })
 );
